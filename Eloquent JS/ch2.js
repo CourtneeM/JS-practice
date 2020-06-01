7b27344b6221fb7a58d573516a85054a1f0b4380
Courtnee McCollum <courtneemeaghan@gmail.com>
2020-06-01 13:14:09 -0500

    Add Chessboard problem

--- Eloquent JS/ch2.js
+++ Eloquent JS/ch2.js
@@ -20,5 +20,26 @@ for (let i = 0; i <= 100; i++) {
 }
 
 
-//
+//Chessboard
 
+let rowWidth = 8;
+let rowHeight = 8;
+let row = "";
+for (let i = 0; i < rowHeight; i++) {
+  for (let j = 0; j < rowWidth; j++) {
+    if (i % 2 === 0) {
+      if (j % 2 === 0) {
+        row += " ";
+      } else {
+        row += "#";  
+      }   
+    } else {
+      if (j % 2 === 0) {
+        row += "#";
+      } else {
+        row += " ";
+      }
+    }
+  }
+  row += "\n";
+}
