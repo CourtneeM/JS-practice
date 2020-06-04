084a329f17ec00e83b0d607e4bd307b659a43919
Courtnee McCollum <courtneemeaghan@gmail.com>
2020-06-04 14:34:27 -0500

    Add alternate solution without Array.reduce()

--- TOP/sumAll/sumAll.js
+++ TOP/sumAll/sumAll.js
@@ -22,4 +22,38 @@ function sumAll(a, b) {
   return(arr.reduce((c, a) => a + c));
 }
 
+
+/* Alternate solution without Array.reduce()
+
+function sumAll(a, b) {
+  let arr = [];
+
+  if (a < 0 || b < 0) {
+    return "ERROR";
+  }
+
+  if (typeof a != "number" || typeof b != "number") {
+    return "ERROR";
+  }
+
+  if (b < a) {
+    let sum = b;
+    for (let i = b + 1; i <= a; i++){
+      sum += i;
+    }
+    return sum;
+  }
+  
+  let sum = a;
+  for (let i = a + 1; i <= b; i++){
+    sum += i;
+  }
+  return sum;
+}
+
+*/
+
+
+
+
 module.exports = sumAll
