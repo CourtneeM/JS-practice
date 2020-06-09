63ddd07a968b29b73508f3a67faf3863d816aa4d
Courtnee McCollum <courtneemeaghan@gmail.com>
2020-06-09 08:51:13 -0500

    Add calculator challenge and tests

---
+++ TOP/calculator/calculator.js
@@ -0,0 +1,44 @@
+function add (a, b) {
+	return a + b;
+}
+
+function subtract (a, b) {
+	return a - b;
+}
+
+function sum (arr) {
+  let total = 0;
+  for(let n in arr) {
+    total += arr[n];
+  }
+  return total;
+}
+
+function multiply (arr) {
+  let total = 1;
+  for(let n in arr) {
+    total *= arr[n];
+  }
+  return total;
+}
+
+function power(a, b) {
+	return a**b;
+}
+
+function factorial(a) {
+  let total = 1;
+  for(let i = 1; i <= a; i++) {
+    total *= i;
+  }
+  return total;
+}
+
+module.exports = {
+	add,
+	subtract,
+	sum,
+	multiply,
+    power,
+	factorial
+}
